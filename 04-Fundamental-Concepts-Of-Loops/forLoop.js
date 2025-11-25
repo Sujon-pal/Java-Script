d77454ee2b2482a573e0ec5616fe4a48077cf1a8
Sujon Pal <dev.sujonpal@gmail.com>
2025-11-25 12:53:37 +0600

    add loop

--- 04-Fundamental-Concepts-Of-Loops/forLoop.js
+++ 04-Fundamental-Concepts-Of-Loops/forLoop.js
@@ -1,65 +1,66 @@
-
 //  Print numbers from 1 to 10
 
 for (let i = 1; i <= 10; i++) {
-    // console.log(i);
+  // console.log(i);
 }
-
-
 
 // Example: Add numbers from 1 to 5
 
 let sum = 0;
 
 for (let i = 1; i <= 5; i++) {
-    sum += i; 
+  sum += i;
 }
 
-console.log(sum); 
-
+// console.log(sum);
 
 // Example: Looping through an array
 
 const fruits = ["Apple", "Banana", "Mango", "Orange"];
 
 for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i]);
+  // console.log(fruits[i]);
 }
-
 
 // Example: Stop loop when number reaches 5
 
 for (let i = 1; i <= 10; i++) {
+  if (i === 5) {
+    break;
+  }
 
-    if (i === 5) {
-        break; 
-    }
-
-    console.log(i);
+  // console.log(i);
 }
 
-// Example: Skip number 
+// Example: Skip number
 
 for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
 
-    if (i === 5) {
-        continue; // skip this iteration
-    }
-
-    console.log(i);
+  console.log(i);
 }
-
 
 // Example: Nested loop to print a star pattern
 
 for (let i = 1; i <= 5; i++) {
-    let stars = "";
+  let stars = "";
 
-    for (let j = 1; j <= i; j++) {
-        stars += "* ";
-    }
+  for (let j = 1; j <= i; j++) {
+    stars += "* ";
+  }
 
-    console.log(stars);
+  console.log(stars);
 }
 
-
+let n = 10;
+let total = 0;
+while (true) {
+  total = total + 1;
+  n++;
+  if (total === 20) {
+    break;
+  }
+}
+console.log(total);
