c847c8c3e4ff39251c1a880601691b40b97cdfb3
Sujon Pal <dev.sujonpal@gmail.com>
2025-11-28 23:57:57 +0600

    add HW

--- 06-Fundamental-Concepts-Of-Array[part-2]/HW.js
+++ 06-Fundamental-Concepts-Of-Array[part-2]/HW.js
@@ -82,7 +82,87 @@ for (let i = work.length - 1; i >= 0; i--) {
 
 console.log(reverce.join(""));
 
-/**/
-/**/
-/**/
+/*
+Task 5
+Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+
+Input: [1, 2, 3]
+
+Expected Output:
+
+Original: [1, 2, 3] Copy: [99, 2, 3]
+
+*/
+
+// Solution 1
+// const arr = [1, 2, 3];
+
+// const copy = [...arr];
+// copy[0] = 99;
+// console.log("Original:", arr);
+// console.log("Copy:", copy);
+
+// Solution 2
+
+// const arr = [1, 2, 3];
+
+// const copy = arr.slice();
+// copy[0]=99;
+// console.log("Original:", arr);
+// console.log("Copy:", copy);
+
+const arr = [1, 2, 3];
+const copy = [];
+
+for (let i = 0; i < arr.length; i++) {
+  copy.push(arr[i]);
+}
+
+copy[0] = 99;
+
+console.log("Original:", arr);
+console.log("Copy:", copy);
+
+/*
+
+Task 6
+Given an array of student objects, print each student’s name and marks.
+
+Expected ooutput:
+John scored 85
+Alice scored 90
+
+*/
+
+let userInfo = [
+  { name: "John", marks: 85 },
+  { name: "Alice", marks: 90 },
+];
+// Solution 1
+// console.log(userInfo[0].name, "Scored ", userInfo[0].marks);
+// console.log(userInfo[1].name, "Scored ", userInfo[1].marks);
+
+// Solution 2
+
+
+
+/*
+Task 7
+Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+input:
+
+[
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]
+Expected Array:
+
+[
+  [1, 2],
+  [99, 4],
+  [5, 6]
+]
+*/
 /**/
