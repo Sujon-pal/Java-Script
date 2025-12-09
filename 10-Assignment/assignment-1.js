bded930982309bc3db4d913be35b880167560049
Sujon Pal <dev.sujonpal@gmail.com>
2025-12-09 19:30:11 +0600

    add assignment

--- 10-Assignment/assignment-1.js
+++ 10-Assignment/assignment-1.js
@@ -1,7 +1,17 @@
+// Find cash out charge
+
 function cashOut(amount) {
+  if (typeof amount !== "number" || amount < 0) {
+    return "Invalid";
+  }
+
+  else {
     let chargeRate = 1.75 / 100;
     const charge = amount * chargeRate;
     return charge;
+  }
 }
 
-console.log(cashOut(20000));
+console.log(cashOut("h"));// Invalid
+console.log(cashOut(2000)); 
+console.log(cashOut(-1)); // Invalide
