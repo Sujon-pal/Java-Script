e4cba242dba1476c896b0c7bf06930a17a7658f2
Sujon Pal <dev.sujonpal@gmail.com>
2025-12-15 23:00:15 +0600

    add

--- 11-Bonus-Module/argument.js
+++ 11-Bonus-Module/argument.js
@@ -1,8 +1,8 @@
 function add(num1, num2) {
-    console.log(num1 , num2 );
-    console.log(arguments)
-    console.log(arguments[3])
+  console.log(num1, num2);
+  console.log(arguments); // not array ; array like object
+  console.log(arguments[3]);
 }
 
 // add(12,22)
-add(2,3,4,5,6)
+add(2, 3, 4, 5, 6);
---
+++ 11-Bonus-Module/matching.js
@@ -0,0 +1,9 @@
+let numbers = [22, 33, 44, 66, 77, 88, 99];
+
+// for (let i = 0; i <= numbers.length - 1; i++) {
+//   let number = numbers[i];
+//   console.log(number);
+// }
+
+// another methon
+
